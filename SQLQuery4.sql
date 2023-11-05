db0bee7960eb432f00fa587fd47d24beb07de2a6
Higor Reis <62409965+higorreisvieira@users.noreply.github.com>
2023-11-04 22:31:02 -0300

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,5 @@
+SELECT * FROM person.Person; -- Seleciona todos os dados da tabela person.Person.
+
+SELECT firstName, LastName FROM person.Person; -- Seleciona somente dados da coluna firstName e LastName da tabela person.Person.
+
+SELECT DISTINCT firstName, LastName FROM person.Person; -- DISTINCT é usado para NÂO EXIBIR dados duplicados. 
